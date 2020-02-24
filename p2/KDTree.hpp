a3c41cb8c1d2d2e62ea51fab524510008ef9016b
Dan Bottcher <dbottch1@binghamton.edu>
2020-02-24 18:03:06 -0500

    Rough draft of K-D Tree

--- p2/KDTree.hpp
+++ p2/KDTree.hpp
@@ -10,6 +10,7 @@
 
 namespace cs547 {
     //todo: create helper functions for files, i.e. is valid header for training, is valid query file, etc.
+    //https://rosettacode.org/wiki/K-d_tree#C.2B.2B
     template<typename T, size_t size>
     struct Point {
         Point() = delete;
@@ -46,9 +47,83 @@ namespace cs547 {
     //end class Point
     template<typename T, size_t size>
     struct Tree {
-        using point = Point<T, size>;
-        Tree() {
+        using _point = Point<T, size>;
+        struct Node {
+            //_point point;
+            int index;
+            int axis;
+            Node* children[2];
+            //Node* left;
+            //Node* right;
+            Node() : index(-1), axis(-1) {
+                children[0] = children[1] = nullptr;
+            }
+            /*T get(size_t i) const {
+                return point.get(i);
+            }
+            float distance(const Point& p) const {
+                return point.distance(p);
+            }*/
+        };
+        Node* root;
+        //Node* ideal; //Node* to Node* with best distance
+        float best_distance;
+        //std::vector<Node*> nodes;
+        std::vector<_point> points;
+        //std::vector<_point>
+        Tree() : root(nullptr) {
+        }
+        ~Tree() {
+            clearRecursive(root);
+            points.clear();
+            root = nullptr;
+        }
+        Tree(const std::vector<_point>& p) : root(nullptr), points(points) {
+            std::vector<int> indices(points.size());
+            for(size_t i = 0; i < indices.size(); i++) {
+                indices[i] = i;
+            }
+            root = construct(indices, points.size(), 0);
+        }
 
+        bool empty() const {
+            return points.empty();
+        }
+
+        /*size_t size() const {
+            return points.size();
+        }*/
+        float distance() const {
+            return std::sqrt(best_distance);
+        }
+    private:
+        Node* construct(std::vector<int>& indices, size_t num_points, size_t depth) {
+            if(num_points <= 0) {
+                return nullptr;
+            }
+            int axis = depth & size;
+            int mid = (num_points - 1) / 2;
+            //https://en.cppreference.com/w/cpp/algorithm/nth_element
+            std::nth_element(indices, mid, num_points, [&](int l, int r) {
+                return points[l].get(axis) < points[r].get(axis);
+            });
+            Node* n = new Node();
+            n->index = indices[mid];
+            n->axis = axis;
+
+            n->next[0] = construct(indices, mid, depth + 1);
+            n->next[1] = construct(indices, num_points - mid - 1, depth+ 1);
+            return n;
+        }
+        void clearRecursive(Node* n) {
+            if(n == nullptr) {
+                return;
+            }
+            if(n->next[0])
+                clearRecursive(n->next[0]);
+            if(n->next[1])
+                clearRecursive(n->next[1]);
+            delete n;
         }
     };
 }
